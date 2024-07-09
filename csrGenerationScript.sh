3bc56b2c8a0745c203eac5f3fcd852528072d552
Adarsh Kamath <adarshkmt2004@gmail.com>
2024-07-09 18:43:19 +0530

    Wrote a script to generate TLS certificate for the container using container name as CN & in hosts list. Added the self signed certificate of root certificate authority to the trust store of the container, so it can be used for validating the TLS certificates. Finally sign the TLS  certificate using root certificate.

---
+++ csrGenerationScript.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Read environment variable for container name
+CONTAINER_NAME=${CONTAINER_NAME}
+
+# Hardcoded paths for root CA certificate and key
+ROOT_CA="/app/root.pem"
+ROOT_CA_KEY="/app/root-key.pem"
+
+# Create csr.json from template
+cat << EOF > csr.json
+{
+  "hosts": [
+    "${CONTAINER_NAME}"
+  ],
+  "CN": "${CONTAINER_NAME}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": []
+}
+EOF
+# Print the content of csr.json (uncommented line)
+cat csr.json
+# Generate CSR and private key
+cfssl genkey csr.json | cfssljson -bare EndServer
+
+cp /app/root.pem /etc/ssl/certs
+update-ca-certificates
+
+# Sign CSR with root CA
+cfssl sign -ca=${ROOT_CA} -ca-key=${ROOT_CA_KEY} -config=/app/cfssl.json -profile=EndServer EndServer.csr | cfssljson -bare EndServer
+
+echo "CSR and private key generated and signed successfully."
